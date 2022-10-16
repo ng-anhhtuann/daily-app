0f3b1c263c784deba5d136ce6274e338c2641909
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2022-10-16 21:41:24 +0700

    Add splash screen

---
+++ Daily/src/assets/index.js
@@ -0,0 +1,6 @@
+const splash = require('./splash.png');
+const logo = require('./logo.png');
+export const imgs = {
+  splash,
+  logo,
+};
--- Daily/src/frames/Start.js
+++ Daily/src/frames/Start.js
@@ -1,53 +1,87 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import axios from 'axios';
-import {ActivityIndicator, View, Image, Text} from 'react-native';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {
-    user: null,
-    isLoading: false,
-    hasError: false,
-  };
-  async componentDidMount() {
-    console.log('componentDidMount');
-    try {
-      this.setState({isLoading: true});
-      setTimeout(async () => {
-        const response = await axios('https://api.github.com/users/facebook');
-        this.setState({user: response.data, isLoading: false});
-      }, 5000);
-    } catch (err) {
-      this.setState({hasError: true, isLoading: false});
-    }
-  }
-
-  render() {
-    const hasUSer = !this.state.isLoading && this.state.user;
-    console.log(this.state);
-    return (
-      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-        {this.state.isLoading && (
-          <View>
-            <ActivityIndicator animating={true} />
-            <Text>Loading...</Text>
-          </View>
-        )}
-        {hasUSer && (
-          <View>
-            <Text>{this.state.user.name}</Text>
-            <Image
-              source={this.state.user.avatar_url}
-              style={{height: 100, width: 100}}
-            />
-          </View>
-        )}
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Text} from 'react-native-ui-lib';
+import {imgs} from '../assets';
+import {BLUE_HEAVY, WHITE} from '../utils/colors';
+import {DeviceWidth, DeviceHeight} from '../utils/device';
+const Start = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgContainer}>
+        <Image source={imgs.splash} style={styles.img} resizeMode="contain" />
       </View>
-    );
-  }
-}
-
-export default App;
+      <View style={styles.header}>
+        <Image source={imgs.logo} style={styles.logo} resizeMode="contain" />
+        <Text numberOfLines={2} center styles={styles.motto}>
+          {`Plan what you will do to be more organized for today, tomorrow and beyond`}
+        </Text>
+      </View>
+      <View styles={styles.buttons}>
+        <TouchableOpacity style={styles.buttonLogin}>
+          <Text style={styles.login}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonSignup}>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: WHITE,
+  },
+  login: {
+    color: WHITE,
+  },
+  buttonLogin: {
+    backgroundColor: BLUE_HEAVY,
+    width: DeviceWidth * 0.8,
+    borderRadius: 8,
+    padding: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonSignup: {
+    width: DeviceWidth * 0.8,
+    borderRadius: 8,
+    padding: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    height: DeviceHeight * 0.3,
+    width: DeviceWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  motto: {
+    textAlign: 'center',
+  },
+  buttons: {
+    height: DeviceHeight * 0.2,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  img: {
+    width: DeviceWidth * 0.8,
+    height: DeviceWidth * 0.8,
+  },
+  logo: {
+    width: DeviceWidth * 0.5,
+    height: DeviceWidth * 0.1,
+    marginBottom: 16,
+  },
+  imgContainer: {
+    height: DeviceHeight * 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+});
+export default Start;
