35e6d5c1308097668f97d632b3bc3be55d167714
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-09 21:28:56 +0700

    Config redux

--- Daily/src/redux/config/stores.js
+++ Daily/src/redux/config/stores.js
@@ -1,9 +1,18 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import testReducer from '../reducers';
-import {applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 const rootReducer = combineReducers({testReducer});
-const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
+
+const persistConfig = {
+  key: 'root',
+  storage,
 };
-export default configureStore;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+export const persistor = persistStore(store);
